09f40af9e03949c2acd3e0c3278ce6b475d850d7
user151509 <user151509@74-80-245-10.ic2net.net>
2019-10-09 12:58:07 -0700

    Fixed fetch method

--- src/actions/fetch.ts
+++ src/actions/fetch.ts
@@ -1,18 +1,35 @@
 import { FETCH_START, FETCH_SUCCESS, FETCH_ERROR } from "./types";
-
-import { request } from "../reducers/fetchReducer";
+import CocktailService from "../provider/services/CocktailService";
 
 export const fetchStart = () => {
   type: FETCH_START;
 };
 
-export const fetch = () => {
+export const fetchSuccess = data => {
+  type: FETCH_SUCCESS;
+  data;
+};
+
+export const fetchError = e => {
+  type: FETCH_ERROR;
+  e;
+};
+
+export function fetch() {
   return async (dispatch, getState) => {
-    const fetchIsLoading = getState().fetchReducer.fetchIsLoading;
-    if (!fetchIsLoading) {
-      const response = await request;
-      console.log(response);
-      dispatch(fetchStart());
+    try {
+      const fetchIsLoading = getState().fetchReducer.fetchIsLoading;
+      if (!fetchIsLoading) {
+        dispatch(fetchStart());
+        let cocktails: any = await CocktailService.getCocktails();
+        let infoCocktails: any = await CocktailService.getAllCocktailWithInfo(
+          cocktails.drinks
+        );
+        let allCocktails = await Cocktail.prepareToSave(infoCocktails);
+        dispatch(fetchSuccess(allCocktails));
+      }
+    } catch (e) {
+      dispatch(fetchError(e));
     }
   };
-};
+}
