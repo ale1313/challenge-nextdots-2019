9644766bcf34237b6b96e508ef56ceec0f5a3200
user151509 <user151509@74-80-245-10.ic2net.net>
2019-10-09 12:29:03 -0700

    Added drinks' list renderer

---
+++ src/components/DrinksRenderer/DrinksRenderer.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Text, View, FlatList, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+
+import ListItem from "../ListItem/";
+
+const DrinksRenderer = props => {
+  if (props.data) {
+    return (
+      <FlatList
+        data={props.data.drinks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <ListItem drinkName={item.strDrink} drinkImage={item.strDrinkThumb} />
+        )}
+      />
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.listEmptyText}>
+          Please search your favourite drink
+        </Text>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  listEmptyText: {
+    fontSize: 30,
+    color: "#FFF",
+    textAlign: "center"
+  }
+});
+
+const mapStateToProps = (state: any) => {
+  return {
+    data: state.fetchReducer.data
+  };
+};
+
+export default connect(mapStateToProps)(DrinksRenderer);
---
+++ src/components/ListItem/ListItem.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Text, Image, View, StyleSheet } from "react-native";
+
+interface Props {
+  drinkImage: string;
+  drinkName: string;
+}
+
+const ListItem = (props: Props) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        resizeMode="contain"
+        source={props.drinkImage}
+        style={styles.drinkImage}
+      />
+      <Text style={styles.drinkName}>{props.drinkName}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#C0C0C0",
+    margin: 3
+  },
+  drinkImage: {
+    width: 30,
+    height: 30,
+    marginRight: 5
+  },
+  drinkName: {
+    fontSize: 30,
+    color: "#800000"
+  }
+});
+
+export default ListItem;
