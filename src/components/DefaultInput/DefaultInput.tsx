5ea391fa21ef7d9b4784c160fa240970a1c46006
user151509 <user151509@74-80-245-10.ic2net.net>
2019-10-09 12:59:03 -0700

    Changed styling

--- src/components/DefaultInput/DefaultInput.tsx
+++ src/components/DefaultInput/DefaultInput.tsx
@@ -3,16 +3,22 @@ import { TextInput, StyleSheet } from "react-native";
 
 interface Props {
   placeholder: string;
+  style: [];
 }
 
 const DefaultInput = (props: Props) => {
-  return <TextInput placeholder={props.placeholder} style={styles.textInput} />;
+  return (
+    <TextInput
+      placeholder={props.placeholder}
+      style={[styles.textInput, props.style]}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
   textInput: {
     width: "100%",
-    height: "8%",
+    height: "10%",
     borderColor: "#000",
     borderWidth: 2,
     borderRadius: 10,
