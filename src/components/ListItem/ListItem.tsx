336a9071ddefb5539ba80b21d641c1fb25301b46
user151509 <user151509@74-80-245-10.ic2net.net>
2019-10-14 08:48:37 -0700

    Updated styling

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -8,8 +8,22 @@ interface Props {
 
 const ListItem = (props: Props) => {
   let { drinkImage, drinkName } = props;
+  const colorPicker = () => {
+    const colors = [
+      "#FFE4C4",
+      "#E6E6FA",
+      "#6495ED",
+      "#98FB98",
+      "#F0E68C",
+      "#DA70D6",
+      "#D2B48C",
+      "#FA8072"
+    ];
+    let randomNumber = Math.floor(Math.random() * 8);
+    return colors[randomNumber];
+  };
   return (
-    <View style={styles.container}>
+    <View style={[{ backgroundColor: colorPicker() }, styles.container]}>
       <Image
         resizeMode="contain"
         source={{ uri: drinkImage }}
@@ -25,7 +39,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignSelf: "center",
     alignItems: "center",
-    backgroundColor: "#FFF",
     margin: 6,
     height: 150,
     width: "95%",
@@ -40,7 +53,7 @@ const styles = StyleSheet.create({
   drinkName: {
     fontSize: 35,
     color: "#800000",
-    maxWidth: "80%",
+    maxWidth: "60%",
     fontFamily: Platform.OS === "ios" ? "Hoefler Text" : "Roboto"
   }
 });
