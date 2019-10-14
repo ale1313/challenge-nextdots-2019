9f73f258993d5ca303126aead453a6dedbc6dafe
user151509 <user151509@74-80-245-10.ic2net.net>
2019-10-14 08:48:11 -0700

    Indexed exports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as DefaultInput } from "./DefaultInput";
+export { default as DefaultText } from "./DefaultText";
+export { default as DrinksRenderer } from "./DrinksRenderer";
+export { default as ListItem } from "./ListItem";
+export { default as SearchButton } from "./SearchButton";
