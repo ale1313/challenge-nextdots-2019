0e1f07dccedf4426d872e318756513b239532fb3
user151509 <user151509@74-80-245-10.ic2net.net>
2019-10-16 11:48:05 -0700

    Re-implemented spread operator

--- src/reducers/drinksReducer.ts
+++ src/reducers/drinksReducer.ts
@@ -22,20 +22,23 @@ const drinksReducer = (state = initialState, action: any) => {
   let { type, data, err } = action;
   switch (type) {
     case GET_DRINKS_START:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         fetchIsLoading: true,
         inputText: data
-      });
+      };
     case GET_DRINKS_SUCCESS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         fetchIsLoading: false,
         data
-      });
+      };
     case GET_DRINKS_ERROR:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         fetchIsLoading: false,
         fetchError: err
-      });
+      };
     default:
       return state;
   }
